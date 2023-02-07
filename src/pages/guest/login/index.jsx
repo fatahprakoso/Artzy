64a713bd65ba95b98f31ffe9843dde881f248e7c
fatahprakoso <67370171+fatahprakoso@users.noreply.github.com>
2023-02-07 11:21:37 +0700

    iupdate: implements splash screen on login page

--- src/pages/guest/login/index.jsx
+++ src/pages/guest/login/index.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
+
+import SplashScreen from "../../general/splash";
+
 /**
  * A React Component that represents login page and its behaviors
  * @returns {component} Login page that what guest user would see
  */
 const Login = () => {
-  return <div style={{ color: "white" }}>It's Login</div>;
+  const [isSplash, setIsSplash] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsSplash(() => false);
+    }, 500);
+  }, []);
+
+  if (isSplash) {
+    return <SplashScreen />;
+  }
+
+  return <div>It's Login</div>;
 };
 
 export default Login;
