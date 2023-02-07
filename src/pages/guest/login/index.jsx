2b20659da8a89ffd4fc90db48be453710187d21c
fatahprakoso <67370171+fatahprakoso@users.noreply.github.com>
2023-02-07 09:29:09 +0700

    add: pages  folder structure

---
+++ src/pages/guest/login/index.jsx
@@ -0,0 +1,7 @@
+/**
+ * A React Component that represents login page and its behaviors
+ * @returns {component} Login page that what guest user would see
+ */
+const index = () => {
+  return <div>It's Login</div>;
+};
