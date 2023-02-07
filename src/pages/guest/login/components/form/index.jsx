35ccabbd0219e70a89fe918d99ca4b1ac7ff5c04
fatahprakoso <67370171+fatahprakoso@users.noreply.github.com>
2023-02-07 12:42:36 +0700

    update:  login form input field
    add: email field and password field
    
    add: submit button

--- src/pages/guest/login/components/form/index.jsx
+++ src/pages/guest/login/components/form/index.jsx
@@ -1,7 +1,44 @@
+import { useRef } from "react";
+
+import { BsQuestionCircle } from "react-icons/bs";
+
+import styles from "./styles.module.scss";
+
 const LoginForm = () => {
+  const password = useRef();
+
+  const tooglePassword = () => {
+    if (password.current.type == "password") password.current.type = "text";
+    else password.current.type = "password";
+  };
+
   return (
     <>
-      <h3>it's form</h3>
+      <div className={styles.formContainer}>
+        <input
+          type="email"
+          name="email"
+          className={styles.emailInput}
+          placeholder="EMAIL"
+        />
+
+        <div className={styles.passwordContainer}>
+          <input
+            type="password"
+            name="password"
+            className={styles.passwordInput}
+            placeholder="PASSWORD"
+            ref={password}
+          />
+          <BsQuestionCircle
+            onClick={tooglePassword}
+            className={styles.tooglePassword}
+            size={22.5}
+          />
+        </div>
+
+        <button className={styles.buttonLogin}>LOG IN</button>
+      </div>
     </>
   );
 };
